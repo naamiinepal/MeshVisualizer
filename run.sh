5773fd145700cb2299a9d020ff542c864a287555
msrepo <mahesh.shakya@naamii.org.np>
2022-08-05 17:44:23 +0545

    fix: file count

--- run.sh
+++ run.sh
@@ -13,27 +13,8 @@ do
 done
 
 echo "searching for ply files in $basedir"
-total_files=$(ls -l ${inputdir}/*.ply | wc -l)
+total_files=$(ls -l ${basedir}/*.ply | wc -l)
 echo "found ${total_files} files"
-
-spin() {
-    sp='/-\|'
-    printf ' '
-    while true; do
-        printf '\b%.1s' "$sp"
-        sp=${sp#?}${sp%???}
-        sleep 0.05
-    done
-}
-
-progressbar()
-{
-    bar="##################################################"
-    barlength=${#bar}
-    n=$(($1*barlength/100))
-    printf "\r[%-${barlength}s (%d%%)] " "${bar:0:n}" "$1" 
-}
-
 
 # convert relative path to absolute path, if required
 inputdir=$(readlink -f $basedir)
@@ -43,10 +24,10 @@ mkdir -p ${basedir}/${outputdir}
 count=0
 for filename in $inputdir/*.ply
 do
-  (( count++ ))
+  count=$((count+1))
   file_id=$(basename $filename)
   extension="${file_id##*.}"
   file_stem="${file_id%.*}"
-  echo  "(${count}/${total_files})writing to ${basedir}/${outputdir}/${file_stem}.ply"
+  echo  "(${count}/${total_files})writing to ${basedir}/${outputdir}/${file_stem}.png"
   docker run --rm -v ${inputdir}:/app/data vedo-test-local:latest python save_screenshot.py /app/data/${file_id}  /app/data/${outputdir}/${file_stem}.png 1> /dev/null 2> /dev/null
 done
