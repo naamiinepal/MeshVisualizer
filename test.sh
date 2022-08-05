56e7f2ac052f18bdcbf05f2183534aea044da645
msrepo <mahesh.shakya@naamii.org.np>
2022-08-05 17:44:59 +0545

    add prompt

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 docker build -t vedo-test-local .
-docker run --rm -v output:/app/data vedo-test-local python test.py
+inputdir=$(readlink -f output)
+echo "writing a test output to ${inputdir}"
+docker run --rm -v $inputdir:/app/data vedo-test-local python test.py 1> /dev/null 2> /dev/null
 
 # docker save vedo-test-local:latest | gzip -c > vedo_latestv3Aug.tar.gz
